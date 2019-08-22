22c6272033745bc8af52dc2a32e11492c14667e7
Lester Hurtado <lester@hurtado.ph>
2019-08-22 22:30:27 +0800

    refactor redeem_voucher_action

--- app/CommandBus/RedeemVoucherAction.php
+++ app/CommandBus/RedeemVoucherAction.php
@@ -7,20 +7,24 @@ use App\CommandBus\Handlers\RedeemVoucherHandler;
 
 class RedeemVoucherAction extends BaseAction
 {
+    protected $permission = 'send message';
+
     public function __invoke(string $path, array $values)
     {
-        $this->bus->dispatch(RedeemVoucherCommand::class, $this->getData($values));
+        if (! $origin = $this->permittedContact()) return;
+
+        $data = array_merge($values, compact('origin'));
+
+        $this->assumeListenerRole($data);
+    }
+
+    protected function assumeListenerRole(array $data)
+    {
+        $this->bus->dispatch(RedeemVoucherCommand::class, $data);
     }
 
     protected function addBusHandlers()
     {
         $this->bus->addHandler(RedeemVoucherCommand::class, RedeemVoucherHandler::class);
-    }
-
-    protected function getData(array $values)
-    {
-        $origin = $this->router->missive->getContact();
-
-        return array_merge($values, compact('origin'));
     }
 }
