d4b44e92c5d17994473bd026c3578d7626f0d66e
Lester Hurtado <lester@hurtado.ph>
2019-08-23 08:15:21 +0800

    refactor code and email regex in sms router

--- routes/sms.php
+++ routes/sms.php
@@ -10,8 +10,11 @@ $router->register('BROADCAST {message}', app(BroadcastAction::class));
 
 $router->register('PING', app(PingAction::class));
 
-//$allowed = config('vouchers.characters');
-//$email = '[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})';
-$router->register('{code=(.*)-(.*)} {email=(.*)}', app(RedeemAction::class));
-
 $router->register('LISTEN {tags}', app(ListenAction::class));
+
+$regex_code = '';
+tap(config('vouchers.characters'), function ($allowed) use (&$regex_code) {
+    $regex_code = "([{$allowed}]{4})-([{$allowed}]{4})";
+});
+$regex_email = '[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})';
+$router->register("{code={$regex_code}} {email={$regex_email}}", app(RedeemAction::class));
