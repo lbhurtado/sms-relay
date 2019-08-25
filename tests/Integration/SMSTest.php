590d68f2b540e202189b50f92c735e2e68513156
Lester Hurtado <lester@hurtado.ph>
2019-08-25 11:15:48 +0800

    add sms integration test

---
+++ tests/Integration/SMSTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Integration;
+
+use App\Contact;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SMSTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('db:seed', ['--class' => 'RoleSeeder']);
+    }
+
+    /** @test */
+    public function sender_of_any_message_becomes_a_subscriber()
+    {
+        /*** arrange ***/
+        $from = '09171111111'; $to = '09182222222'; $message = $this->faker->sentence;
+
+        /*** act ***/
+        $response = $this->json($this->method, $this->uri, compact('from', 'to', 'message'));
+        usleep(500);
+
+        /*** assert ***/
+        $response->assertStatus(200);
+        tap(Contact::bearing($from), function ($contact) {
+            $this->assertTrue($contact->hasRole('subscriber'));
+            $this->assertFalse($contact->hasRole('listener'));
+            $this->assertFalse($contact->hasRole('spokesman'));
+        });
+    }
+}
