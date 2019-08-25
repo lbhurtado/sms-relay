1e311df9bda62385c6f1356cd81761766f417e27
Lester Hurtado <lester@hurtado.ph>
2019-08-25 11:45:13 +0800

    check invalid code in redeemvoucher test

--- tests/Feature/RedeemVoucherTest.php
+++ tests/Feature/RedeemVoucherTest.php
@@ -19,6 +19,49 @@ class RedeemVoucherTest extends TestCase
     }
 
     /** @test */
+    public function invalid_code_does_make_a_sender_become_a_spokesman_or_listener()
+    {
+        /*** arrange ***/
+        $code = $this->faker->word;
+        $email = $this->faker->email;
+        $from = '09171111111'; $to = '09182222222'; $message = "{$code} {$email}";
+
+        /*** act ***/
+        $response = $this->json($this->method, $this->uri, compact('from', 'to', 'message'));
+        usleep(500);
+
+        /*** assert ***/
+        $response->assertStatus(200);
+        tap(Contact::bearing($from), function ($contact) {
+            $this->assertTrue($contact->hasRole('subscriber'));
+            $this->assertFalse($contact->hasRole('listener'));
+            $this->assertFalse($contact->hasRole('spokesman'));
+        });
+    }
+
+    /** @test */
+    public function with_valid_code_subscriber_no_more_then_email_from_message_becomes_email_of_sender()
+    {
+        /*** arrange ***/
+        $code = $this->getVoucherCode('listener');
+        $email = $this->faker->email;
+        $from = '09171111111'; $to = '09182222222'; $message = "{$code} {$email}";
+
+        /*** act ***/
+        $response = $this->json($this->method, $this->uri, compact('from', 'to', 'message'));
+        usleep(500);
+
+        /*** assert ***/
+        $response->assertStatus(200);
+        $contact = tap(Contact::bearing($from), function ($contact) {
+            $this->assertFalse($contact->hasRole('subscriber'));
+            $this->assertTrue($contact->hasRole('listener'));
+            $this->assertFalse($contact->hasRole('spokesman'));
+        });
+        $this->assertEquals($email, $contact->email);
+    }
+
+    /** @test */
     public function sender_was_a_subscriber_becomes_an_spokesman()
     {
         /*** arrange ***/
