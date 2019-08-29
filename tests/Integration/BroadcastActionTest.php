bcf23d3405b497a9740d8807d3ff7a99487745aa
Lester Hurtado <lester@hurtado.ph>
2019-08-29 22:11:28 +0800

    add negative testing in BroadcastAction

--- tests/Integration/BroadcastActionTest.php
+++ tests/Integration/BroadcastActionTest.php
@@ -17,50 +17,90 @@ class BroadcastActionTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @var SMS */
-    protected $sms;
+    protected $contact;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $this->artisan('db:seed', ['--class' => 'RoleSeeder']);
-
-        //this needs to be assigned to a class variable, scoping issues with service layer
-        $this->sms = $this->createBroadcastSMSBySpokesman();
     }
 
     /** @test */
-    public function broadcast_action_sends_to_all_contacts_except_self_but_receives_feedback()
+    public function spokesman_broadcast_action_sends_to_all_contacts_except_self_but_receives_feedback()
     {
         /*** arrange ***/
         Notification::fake();
         $this->createSubscribers($count = 2);
         $message = $this->faker->sentence;
+        //this needs to be assigned to a class variable, scoping issues with service layer
+        $sms = $this->createBroadcastSMS('spokesman');
 
         /*** act ***/
         app(BroadcastAction::class)->__invoke('', compact('message'));
 
         /*** assert ***/
-        Contact::whereNotIn('mobile',[$this->sms->origin->mobile])->get()->each(function ($contact) use ($message, &$i) {
+        Contact::whereNotIn('mobile',[$sms->origin->mobile])->get()->each(function ($contact) use ($message, &$i) {
             ++$i;
             Notification::assertSentTo($contact, Broadcast::class, function ($notification) use ($contact, $message) {
                 return Broadcast::getFormattedMessage($contact, $message) == $notification->getContent($contact);
             });
         });
         $this->assertEquals($i, $count);
-        tap(Contact::bearing($this->sms->origin->mobile), function ($spokesman) use ($message) {
+        tap(Contact::bearing($sms->origin->mobile), function ($spokesman) use ($message) {
             Notification::assertSentTo($spokesman, Feedback::class, function ($notification) use ($spokesman, $message) {
                 return Feedback::getFormattedMessage($spokesman, $message) == $notification->getContent($spokesman);
             });
         });
     }
 
-    protected function createBroadcastSMSBySpokesman(): \LBHurtado\Missive\Models\SMS
+    /** @test */
+    public function listener_broadcast_action_will_not()
+    {
+        /*** arrange ***/
+        Notification::fake();
+        $message = $this->faker->sentence;
+        $sms = $this->createBroadcastSMS('listener');
+
+        /*** act ***/
+        app(BroadcastAction::class)->__invoke('', compact('message'));
+
+        /*** assert ***/
+        Contact::whereNotIn('mobile',[$sms->origin->mobile])->get()->each(function ($contact) use ($message, &$i) {
+            ++$i;
+            Notification::assertNotSentTo($contact, Broadcast::class);
+        });
+        tap(Contact::bearing($sms->origin->mobile), function ($listener) use ($message) {
+            Notification::assertNotSentTo($listener, Feedback::class);
+        });
+    }
+
+    /** @test */
+    public function subscriber_broadcast_action_will_not()
+    {
+        /*** arrange ***/
+        Notification::fake();
+        $message = $this->faker->sentence;
+        $sms = $this->createBroadcastSMS('subscriber');
+
+        /*** act ***/
+        app(BroadcastAction::class)->__invoke('', compact('message'));
+
+        /*** assert ***/
+        Contact::whereNotIn('mobile',[$sms->origin->mobile])->get()->each(function ($contact) use ($message, &$i) {
+            ++$i;
+            Notification::assertNotSentTo($contact, Broadcast::class);
+        });
+        tap(Contact::bearing($sms->origin->mobile), function ($listener) use ($message) {
+            Notification::assertNotSentTo($listener, Feedback::class);
+        });
+    }
+
+    protected function createBroadcastSMS(string $role): \LBHurtado\Missive\Models\SMS
     {
         $from = '+639191234567';
         $sms = factory(SMS::class)->create(compact('from'));
-        $this->createSpokesman($from);
+        $this->createContact($from, $role);
 
         $missive = app(Missive::class)->setSMS($sms);
         (new Router($missive))->process($sms);
@@ -68,11 +108,11 @@ class BroadcastActionTest extends TestCase
         return $sms;
     }
 
-    protected function createSpokesman(string $mobile)
+    protected function createContact(string $mobile, string $role)
     {
-        $this->spokesman = factory(Contact::class)
+        factory(Contact::class)
             ->create(compact('mobile'))
-            ->syncRoles('spokesman')
+            ->syncRoles($role)
         ;
 
         return $this;
