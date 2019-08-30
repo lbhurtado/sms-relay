69b8586585e2f4c53c5b62e937906a3749235f0e
Lester Hurtado <lester@hurtado.ph>
2019-08-30 14:35:48 +0800

    include no-hashtag messages to test in RelayActionTest

--- tests/Integration/RelayActionTest.php
+++ tests/Integration/RelayActionTest.php
@@ -28,7 +28,7 @@ class RelayActionTest extends TestCase
     }
 
     /** @test */
-    public function subscriber_relay_action_dispatches_relayed_notifications()
+    public function subscriber_with_hashtags_relay_action_dispatches_relayed_notifications()
     {
         /*** arrange ***/
         Notification::fake();
@@ -77,6 +77,65 @@ class RelayActionTest extends TestCase
         Notification::assertSentTo($listener2, Hashtags::class);
         Notification::assertSentTo($listener3, Hashtags::class);
         Notification::assertSentTo($listener4, Hashtags::class);
+        Notification::assertNotSentTo($listener5, Hashtags::class);
+        $mobiles = Setting::get('forwarding.mobiles');
+        foreach ($mobiles as $mobile) {
+            $contact = Contact::bearing($mobile);
+            Notification::assertSentTo($contact, Forwarded::class);
+        }
+        Notification::assertSentTo(Contact::bearing($sms->origin->mobile), Relayed::class);
+    }
+
+    /** @test */
+    public function subscriber_without_hashtags_relay_action_dispatches_relayed_notifications()
+    {
+        /*** arrange ***/
+        Notification::fake();
+        $spokesman = Contact::create(['mobile' => '09654444444'])
+            ->syncRoles('spokesman')
+            ->setEmail('spokesman@lgu.gov.ph')
+            ->catch(['tag1'])
+        ;
+        $listener1 = Contact::create(['mobile' => '09108888888'])
+            ->syncRoles('listener')
+            ->setEmail('lister1@@lgu.gov.ph')
+            ->catch(['tag1', 'tag3'])
+        ;
+        $listener2 = Contact::create(['mobile' => '09209999999'])
+            ->syncRoles('listener')
+            ->setEmail('listener2@lgu.gov.ph')
+            ->catch(['tag1'])
+        ;
+        $listener3 = Contact::create(['mobile' => '09307777777'])
+            ->syncRoles('listener')
+            ->setEmail('listener3@lgu.gov.ph')
+            ->catch(['tag2'])
+        ;
+        $listener4 = Contact::create(['mobile' => '09456666666'])
+            ->syncRoles('listener')
+            ->setEmail('listener4@lgu.gov.ph')
+            ->catch(['tag3'])
+        ;
+        $listener4->catch(['tag2']);
+        $listener5 = Contact::create(['mobile' => '09555555555'])
+            ->syncRoles('listener')
+            ->setEmail('listener5@lgu.gov.ph')
+            ->catch(['tag3'])
+        ;
+
+        $message = $this->faker->sentence;
+        $sms = $this->prepareToRelayAs('subscriber', $message);
+
+        /*** act ***/
+        app(RelayAction::class)->__invoke('', compact('message'));
+
+        /*** assert ***/
+        Notification::assertSentTo(Contact::bearing($sms->origin->mobile), Acknowledged::class);
+        Notification::assertNotSentTo($spokesman, Hashtags::class);
+        Notification::assertNotSentTo($listener1, Hashtags::class);
+        Notification::assertNotSentTo($listener2, Hashtags::class);
+        Notification::assertNotSentTo($listener3, Hashtags::class);
+        Notification::assertNotSentTo($listener4, Hashtags::class);
         Notification::assertNotSentTo($listener5, Hashtags::class);
         $mobiles = Setting::get('forwarding.mobiles');
         foreach ($mobiles as $mobile) {
