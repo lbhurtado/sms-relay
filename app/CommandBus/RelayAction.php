da80bdd3d5cdf420ccf9ff4b43e2d94d7ee06c6a
Lester Hurtado <lester@hurtado.ph>
2019-09-05 10:40:43 +0800

    broadcast if spokesman and BROADCAST keyword is optional

--- app/CommandBus/RelayAction.php
+++ app/CommandBus/RelayAction.php
@@ -4,23 +4,35 @@ namespace App\CommandBus;
 
 use App\CommandBus\Commands\RelayCommand;
 use App\CommandBus\Handlers\RelayHandler;
+use App\CommandBus\Commands\BroadcastCommand;
+use App\CommandBus\Handlers\BroadcastHandler;
 use App\CommandBus\Middlewares\{LogMiddleware, EmailMiddleware, ReplyMiddleware, ForwardMiddleware};
 
 class RelayAction extends BaseAction
 {
     protected $permission = 'send message';
 
+    protected $broadcastData = [];
+
+    protected $values = [];
+
     public function __invoke(string $path, array $values)
     {
-        if (! $this->permittedContact()) return;
+        if (! tap($this->permittedContact(), function ($origin) use ($values) {
+            if ($origin->hasPermissionTo('send broadcast')) {
+                $this->broadcastData = array_merge($values, compact('origin'));
+            }
+        })) return;
 
-        $go = (object) config('sms-relay.relay');
+        $go = $this->shouldProceed();
 
         $this->log($go->log)
             ->email($go->email)
             ->forward($go->mobile)
             ->reply($go->reply)
-            ->relay($go->hashtags);
+            ->relay($go->hashtags && ! $this->shouldBroadcast())
+            ->broadcast($this->shouldBroadcast())
+            ;
     }
 
     protected function log(bool $go = true)
@@ -58,15 +70,33 @@ class RelayAction extends BaseAction
         return $this;
     }
 
+    protected function broadcast(bool $go = true)
+    {
+        ! $go ||  $this->bus->dispatch(BroadcastCommand::class, $this->broadcastData, $this->getMiddlewares());
+
+        return $this;
+    }
+
     protected function addBusHandlers()
     {
         $this->bus->addHandler(RelayCommand::class, RelayHandler::class);
+        $this->bus->addHandler(BroadcastCommand::class, BroadcastHandler::class);
     }
 
     private function getData()
     {
         return [
-            'sms' => $this->router->missive->getSMS()
+            'sms' => $this->router->missive->getSMS(),
         ];
+    }
+
+    protected function shouldProceed()
+    {
+        return (object) config('sms-relay.relay');
+    }
+
+    protected function shouldBroadcast()
+    {
+        return (bool) config('sms-relay.broadcast.optional') && (bool) $this->broadcastData;
     }
 }
--- config/sms-relay.php
+++ config/sms-relay.php
@@ -2,11 +2,15 @@
 
 return [
     'relay' => [
-        'log'      => env('RELAY_LOG',      true),
-        'email'    => env('RELAY_EMAIL',    true),
-        'mobile'   => env('RELAY_MOBILE',   true),
-        'reply'    => env('RELAY_REPLY',    true),
-        'hashtags' => env('RELAY_HASHTAGS', true),
+        'log'       => env('RELAY_LOG',       true),
+        'email'     => env('RELAY_EMAIL',     true),
+        'mobile'    => env('RELAY_MOBILE',    true),
+        'reply'     => env('RELAY_REPLY',     true),
+        'hashtags'  => env('RELAY_HASHTAGS',  true),
+    ],
+    'broadcast' => [
+        'keyword'   => 'BROADCAST',
+        'optional'  => env('BROADCAST_OPTIONAL', true),
     ],
     'permissions' => [
         'spokesman'  => ['send message', 'issue command', 'send broadcast'],
