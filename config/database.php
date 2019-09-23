583d1601682828f2df58247f0a574dfb9ad58deb
Lester Hurtado <lester@hurtado.ph>
2019-09-24 05:04:23 +0800

    change localhost to 127.0.0.1 in config(database.testing)

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
         ],
         'testing' => [
             'driver' => 'mysql',
-            'host' => env('DB_TEST_HOST', 'localhost'),
+            'host' => env('DB_TEST_HOST', '127.0.0.1'),
             'database' => env('DB_TEST_DATABASE', 'testing_db'),
             'username' => env('DB_TEST_USERNAME', 'root'),
             'password' => env('DB_TEST_PASSWORD', ''),
